eef72a3847c73dc0687746047d8424750c88ace9
Terence <terence@awesapp.com>
2017-11-24 12:08:55 +0800

    expose flatlist ref

--- src/MessageContainer.js
+++ src/MessageContainer.js
@@ -156,6 +156,7 @@ export default class MessageContainer extends React.Component {
         style={styles.container}
       >
         <FlatList
+          ref={(r) => {this._flatListRef = r;}}
           enableEmptySections={true}
           automaticallyAdjustContentInsets={false}
           initialListSize={20}
